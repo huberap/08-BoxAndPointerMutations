1fca11016fc199150456190210229da8997ec22e
huberap <huberap@rose-hulman.edu>
2018-03-21 08:57:07 -0400

    done

--- src/m3_implementing_functions_that_mutate.py
+++ src/m3_implementing_functions_that_mutate.py
@@ -3,8 +3,8 @@ This module lets you practice IMPLEMENTING
 functions that MUTATE their arguments.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Done.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -113,6 +113,11 @@ def run_test_swap_colors():
 
 
 def swap_colors(circle, rectangle):
+
+    temp = circle.fill_color
+    circle.fill_color = rectangle.fill_color
+    rectangle.fill_color=temp
+
     """
     What comes in:
       -- An rg.Circle.
@@ -134,7 +139,7 @@ def swap_colors(circle, rectangle):
       :type rectangle: rg.Rectangle
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE 2. Implement and test this function.
     #          Tests have been written for you (above).
     #
     ####################################################################
